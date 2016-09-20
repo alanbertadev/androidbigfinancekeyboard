78bcec10377467562dcb7c7b18faba02ebfcc61f
Alan Berta <alanbertadev@gmail.com>
2016-09-19 23:13:16 -0400

    Implemented simple pushing and popping of figures on the financial keyboard screen

--- app/src/main/java/com/alanbertadev/bigfinancekeyboard/BigFinanceKeyboard.java
+++ app/src/main/java/com/alanbertadev/bigfinancekeyboard/BigFinanceKeyboard.java
@@ -6,7 +6,15 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import java.util.Stack;
+
 public class BigFinanceKeyboard extends Activity {
+
+    private final static int MAX_CURSOR_POSITION = 11; //100,000,000.00
+
+    private TextView financialOutputTextView;
+
+    private Stack<Integer> figures = new Stack<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,5 +33,54 @@ public class BigFinanceKeyboard extends Activity {
         });
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        financialOutputTextView = (TextView)findViewById(R.id.financialoutput);
+    }
 
+    private boolean pushFigure(final Integer figure) {
+        if(figures.size() < MAX_CURSOR_POSITION) {
+            figures.push(figure);
+            refreshUI();
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private boolean popFigure() {
+        if(figures.empty()) {
+            return false;
+        } else {
+            figures.pop();
+            refreshUI();
+            return true;
+        }
+    }
+
+    private void refreshUI() {
+        StringBuilder value = new StringBuilder();
+        value.append("$");
+        switch(figures.size()) {
+            case 2:
+                value.append("0");
+                break;
+            case 1:
+                value.append("0.0");
+                break;
+            case 0:
+                value.append("0.00");
+                break;
+            default:
+                break;
+        }
+        for(int i=0; i<figures.size(); i++) {
+            if(i==(figures.size()-2)) {
+                value.append(".");
+            }
+            value.append(figures.elementAt(i));
+        }
+        financialOutputTextView.setText(value.toString());
+    }
 }
